c5fe25e32e379798043d7610bf024d96eb7ce73b
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-05-22 22:37:28 +0900

    230522

---
+++ 11/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const my_example_lib_1 = require("my-example-lib");
+console.log(my_example_lib_1.value);
---
+++ 11/version.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getString = void 0;
+function getString() {
+    console.log(`index.d.ts에서 선언한 변수 ${version}`);
+}
+exports.getString = getString;
